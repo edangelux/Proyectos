ef56de30452fab11732512cf36d51b52d9d3e21b
Eddy <eddytorrez34@gmail.com>
2022-01-21 03:56:21 -0600

    aprendimos sobre algunas variables del uso javascript

---
+++ javascript aprendiendo/variables y comentarios/variables.js
@@ -0,0 +1,11 @@
+/* haremos y aprendaremos sobre varibales js */
+/* una variable es un contenedor de informacion */
+
+var país = "Nicaragua"; /* los string en las variables van entre comillas dobles o simples */
+var continente = "Americano" /* esto seria un string igual que el anterior */
+var pais_y_continente = país+ ' ' +continente; /* aca unimos dos variables en una, osea seria una concatenacion o mejor conocido como cadena de texto */
+
+país = "Costa Rica"; /* de esta manera se cambiaria valor a una variable sin necesidad del var */
+
+console.log(país, continente); /* esto lo que haria es resaltar el resultado de las variables en consola de la pagina */
+alert(pais_y_continente); /* luego de el console aparecera el alert que es la cadeba de texto */
