f071e71be72efb467e70ea6aa12899cc71de1d22
Eddy <eddytorrez34@gmail.com>
2022-01-22 02:34:32 -0600

    tipos de datos y la funcion typeof

---
+++ javascript aprendiendo/tipo de datos y operadores/tipos de datos y operadores.js
@@ -0,0 +1,47 @@
+'use strict'
+
+ /* operadores */
+
+ /* adicion  +
+ sustraccion  -
+ multiplicacion  *
+ division  /
+ resto  %
+ 
+ operadores de comparacion
+ 
+ igualdad ==
+ desigualdad !=
+ estrictamente iguales ===
+ estrictamente desiguales !==
+ mayor que >
+ mayor o igual que >=
+ menor que <
+ menor o igual que <=
+ 
+ operadores aritmetico
+ 
+ incremento ++
+ decremento --
+ resto %
+ 
+ operadores ternario
+ 
+ condicion ? valor1 : valor2
+ si la condicion es true tomara el valor1, sino el valor2*/
+var numero1 = 5;
+var numero2 = 10;
+var operacion = numero1 + numero2;
+
+alert(" el resultado de la operacion es: "+operacion); /* aca mostraria en un alert la respuesta total de la suma */
+ 
+/* tipos de datos */
+var number = 10; /* valor numerico puede ser entero o decimal */
+var string = "a"; /* cadena de texto para ingresar texto que representan un valor*/
+var boolean = true  false /* es un valor logico que sus valores son verdadero o falso para hacer condiciones */
+var null /* denota valor nulo */
+var undefined /* es decir un valor sin definir  */
+var symbol /* cuyos datos son unicos o inmutables */
+var object {} /* puede contener mas variables en su interior */
+
+console.log(typeof number); /* typeof en la consola demostrara que tipo de dato esta hecha la variable */
