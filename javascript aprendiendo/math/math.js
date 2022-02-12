2772323f51a665596ce48f46c70842f53ec54819
Eddy <eddytorrez34@gmail.com>
2022-02-11 21:36:51 -0600

    aprendimos objeto math de js

---
+++ javascript aprendiendo/math/math.js
@@ -0,0 +1,13 @@
+/* objeto math en javascript */
+
+let resultado;
+
+resultado = Math.PI; /* te da el resultado de PI */
+resultado = Math.round(2.6) /* redondea el numero*/
+resultado = Math.ceil(2.2) /* siempre redondea hacia arriba */
+resultado = Math.floor(2.9) /* siempre redondeara hacia abajo  */
+resultado = Math.sqrt(144) /* es raiz cuadrada */
+resultado = Math.abs(-200) /* todo numero negativo se pasa a positivo  */
+resultado = Math.min( 3, 2, 4); /* el minimo de los resultados */
+resultado = Math.max( 3, 2, 4); /* el maximo de los resultados */
+resultado = Math.random() /* genera numeros random */
