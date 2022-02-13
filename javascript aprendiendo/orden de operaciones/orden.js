fef5dce1ea0a7cebf6d7a3b3cc1126bd14e5a0b6
Eddy <eddytorrez34@gmail.com>
2022-02-13 04:32:14 -0600

    aprendimos sobre el orden de los operadores y incrementos

---
+++ javascript aprendiendo/orden de operaciones/orden.js
@@ -0,0 +1,17 @@
+/* orden de las operaciones */
+
+let resultado;
+
+resultado = 20 + 30 * 2; /* tomara el mayor peso que la suma entonces sera 30 * 2 y luego + 20 */
+
+resultadosuma = (20 + 30) * 2; /* seria la manera para que primero se sume y luego multiplique */
+
+console.log(resultado);
+
+/* incrementos */
+
+let puntaje = 10;
+puntaje++; //10
+++puntaje; //12
+
+console.log(puntaje)
