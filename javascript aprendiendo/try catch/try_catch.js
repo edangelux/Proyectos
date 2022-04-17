82bfe93bd5db2b74a5ed17524314af5e4f85db4d
Eddy <eddytorrez34@gmail.com>
2022-04-17 17:01:26 -0600

    aprendimos sobre try catch

---
+++ javascript aprendiendo/try catch/try_catch.js
@@ -0,0 +1,13 @@
+/* yo auunque tenga mas de mil lineas de codigos en javascript un error en cualquier linea para el programa 
+la solucion de esto podria ser un try catch*/
+
+try { //intenta hacer lo siguiente
+
+
+} catch(error) { //si hay un error dime cual es el error
+    
+    console.log(error); //el console para saber el error
+
+} 
+
+//esto no se tiene que hacer un habito solo en momentos criticos de un error en algo sumamente importante
