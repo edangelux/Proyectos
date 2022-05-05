fb632d92e2d63335c1b20682175bf0b1009a986a
Eddy <eddytorrez34@gmail.com>
2022-05-05 10:36:30 -0600

    descubrimos el createElement

--- DOM scripting/js/scripts.js
+++ DOM scripting/js/scripts.js
@@ -13,3 +13,21 @@ console.log(enlaces)
 //funciona unicamente seleccionando ids
 const id = document.getElementById('heading')
 console.log(id)
+
+
+
+// Crear html con createElement
+// la mejor manera de como explicarlo es cuando colocas un articulo al carrito de compra se genera un html para que el usuario luego mire el articulo colocado automaticamente el codigo html
+
+const html = document.createElement('A') //falta agregar los atributos, texto y clase 
+// texto, clase, atributo
+ html.href = 'nuevo__enlace.html'
+ html.classList.add('navegacion__enlace')
+ html.textContent = 'nuevo enlace'
+
+ //agregarlo al documento seria asi
+ const nav = document.querySelector('.navegacion')
+ nav.appendChild(html)
+ //osea tomamos esta clase y al tomarla diremos colocate este html
+
+ console.log(html)
