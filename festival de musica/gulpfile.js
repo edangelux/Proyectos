89a31ecb4a960bf563e16398dd5d98ae18679bec
Eddy <eddytorrez34@gmail.com>
2022-06-18 10:05:46 -0600

    instalado gulp y configuracion gulp finalizada

---
+++ festival de musica/gulpfile.js
@@ -0,0 +1,23 @@
+const {src, dest, watch} = require("gulp"); //extraer funcionalidades de gulp.file
+const sass = require("gulp-sass")(require('sass')); //extraer funcionalidades sass descargando extension gulp-sass
+
+//src = buscar dest = almacenar watch = observar cambios
+
+function css (call){
+    src('src/scss/app.scss') //identificar archivo de sass
+
+    .pipe(sass()) // compilarlo
+
+    .pipe(dest('build/css')); //guardarla
+
+    call(); // avisa a gulp cuando llegamos al final
+}
+
+function dev (call){ //observar todo tipo de cambios de la hoja y se pasa a la funcion css
+    watch('src/scss/app.scss', css)
+
+    call();
+}
+
+exports.css = css;
+exports.dev = dev;
