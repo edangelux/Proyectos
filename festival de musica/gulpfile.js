003c42e5beadf863fe56ffb5ac5cd6a27561a538
Eddy <eddytorrez34@gmail.com>
2022-06-18 20:10:09 -0600

    se creo automatizacion de hacer webp y menos pasada con gulp

--- festival de musica/gulpfile.js
+++ festival de musica/gulpfile.js
@@ -1,8 +1,14 @@
-const {src, dest, watch} = require("gulp"); //extraer funcionalidades de gulp.file
-const sass = require("gulp-sass")(require('sass')); //extraer funcionalidades sass descargando extension gulp-sass
+const {src, dest, watch, parallel} = require("gulp"); //extraer funcionalidades de gulp.file
 
+//css
+const sass = require("gulp-sass")(require('sass')); //extraer funcionalidades sass descargando extension gulp-sass
 const plumber = require('gulp-plumber');
 //src = buscar dest = almacenar watch = observar cambios
+
+//imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp')
 
 function css (call){
     src('src/scss/**/*.scss') //identificar archivo de sass
@@ -15,6 +21,31 @@ function css (call){
     call(); // avisa a gulp cuando llegamos al final
 }
 
+function webpp(call){
+    
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe(webp(opciones))
+    .pipe(dest('build/img'))
+
+call()
+}
+    
+function imagenes(call){
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest ('build/img'))
+    
+    call();
+}
+
 function dev (call){ //observar todo tipo de cambios de la hoja y se pasa a la funcion css
     watch('src/scss/**/*.scss', css)
 
@@ -22,4 +53,6 @@ function dev (call){ //observar todo tipo de cambios de la hoja y se pasa a la f
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.webpp = webpp;
+exports.dev = parallel(imagenes, webpp, dev);
